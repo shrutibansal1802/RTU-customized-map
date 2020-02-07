2f4d8bc7f17a5f4c8659d70ec8d1698103ded9f7
shruti <shrutibansal1802@gmail.com>
2020-02-07 18:33:36 +0530

    added markers

--- app/src/main/java/com/example/onclickgmap/MapsActivity.kt
+++ app/src/main/java/com/example/onclickgmap/MapsActivity.kt
@@ -66,11 +66,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(Tpnt).title("T Point"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Tpnt))
 
-        val latLngOrigin = LatLng(25.142490,75.806798) // library
-        mMap.addMarker(MarkerOptions().position(latLngOrigin).title("Library"))
+        val libra = LatLng(25.142490,75.806798) // library
+        mMap.addMarker(MarkerOptions().position(libra).title("Library"))
 
         val Dblock = LatLng(25.144265,75.804995) // D block
         mMap.addMarker(MarkerOptions().position(Dblock).title("D block"))
+
+        val Cblock = LatLng(25.143976,75.805427)//C block
+        mMap.addMarker(MarkerOptions().position(Cblock).title("C Block - Mechanical department"))
+
+
+        val Bblock = LatLng(25.143732,75.808528) // B block
+        mMap.addMarker(MarkerOptions().position(Bblock).title("b block- Civil department"))
+
+
+        val Ablock = LatLng(25.143555,75.806224) // A block
+        mMap.addMarker(MarkerOptions().position(Ablock).title("A block -electrical department"))
 
         val PNB = LatLng(25.143150,75.807046) // PNB
         mMap.addMarker(MarkerOptions().position(PNB).title("Punjab National Bank"))
@@ -81,17 +92,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val Proc = LatLng(25.145161,75.805693) // Proctor
         mMap.addMarker(MarkerOptions().position(Proc).title("Proctor office"))
 
+
+        val Agora = LatLng(25.142775,75.806358) // Proctor
+        mMap.addMarker(MarkerOptions().position(Agora).title("Agora"))
+
         val RMH = LatLng(25.144544,75.805954) // RMH
         mMap.addMarker(MarkerOptions().position(RMH).title("ramanuj hall-RMH"))
 
 
-        val Cblock = LatLng(25.143976,75.805427)//T point
-        mMap.addMarker(MarkerOptions().position(Cblock)
-            .title("C Block - Mechanical department")
-           // .icon(BitmapDescriptorFactory.fromResource(R.drawable.cblock))
-        )
+        val petro = LatLng(25.142439,75.805619) // petro building
+        mMap.addMarker(MarkerOptions().position(petro).title("Petroleum building"))
 
 
+        val CS  = LatLng(25.141700,75.806839)
+        mMap.addMarker(MarkerOptions().position(CS).title("Computer Science Department"))
+
+
+        val dispensary = LatLng(25.141212,75.807399)
+        mMap.addMarker(MarkerOptions().position(dispensary).title("Dispensary"))
+
+
+        val canteen  = LatLng(25.140987,75.807285)
+        mMap.addMarker(MarkerOptions().position(canteen).title("Canteen"))
+
+
+        val girlshostel  = LatLng(25.144544,75.805954)
+        mMap.addMarker(MarkerOptions().position(girlshostel).title("Girls Hostel"))
+
+
+        val pnb_atm = LatLng(25.139682,75.810956)
+        mMap.addMarker(MarkerOptions().position(pnb_atm).title("PNB ATM"))
+
+
+        val main_gate = LatLng(25.139733,75.811074)
+        mMap.addMarker(MarkerOptions().position(main_gate).title("Main Gate"))
+
+
+        val hpoint = LatLng(25.140770,75.809473)
+        mMap.addMarker(MarkerOptions().position(hpoint).title("H- point"))
+
+
+        val gym  = LatLng(25.139913,75.808180)
+        mMap.addMarker(MarkerOptions().position(gym).title("Gymnasium"))
 
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Tpnt, 16f))
 
